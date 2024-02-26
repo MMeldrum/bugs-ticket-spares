5c240d137d37be48c5376ae523388a2905bc071c
Martin Meldrum <martinmeldrum@gmail.com>
2024-02-26 20:29:25 +0000

    first commit

---
+++ app/components/TableButtons.tsx
@@ -0,0 +1,12 @@
+
+import { Button, ButtonGroup } from 'flowbite-react';
+
+function Component() {
+  return (
+    <ButtonGroup>
+      <Button color="gray">Profile</Button>
+      <Button color="gray">Settings</Button>
+      <Button color="gray">Messages</Button>
+    </ButtonGroup>
+  );
+}
